8bdef83caf61269e161ea2be794d11259e0836a1
Jan-Krystof Kujel Zahradnik <jendazah@gmail.com>
2025-09-22 22:25:00 +0200

    chore: add migrations

---
+++ data/migrations/1_init.down.sql
@@ -0,0 +1,7 @@
+drop table if exists address;
+drop table if exists projects;
+drop table if exists accounts;
+
+drop type if exists role;
+
+DROP DATABASE IF EXISTS "rusalka-dev";
---
+++ data/migrations/1_init.up.sql
@@ -0,0 +1,32 @@
+create database "rusalka-dev";
+
+CREATE TABLE address (
+    id           bigserial PRIMARY KEY,
+    street_line1 text        NOT NULL,
+    street_line2 text,
+    city         text        NOT NULL,
+    state        text,
+    postal_code  text,
+    country      text        NOT NULL DEFAULT 'US',
+    created_at   timestamptz NOT NULL DEFAULT now(),
+    updated_at   timestamptz NOT NULL DEFAULT now()
+);
+
+CREATE TYPE role AS ENUM ('admin', 'user', 'customer');
+
+create table accounts (
+    id bigint GENERATED ALWAYS AS IDENTITY,
+    email text not null,
+    first_name text not null,
+    last_name text not null,
+    role role not null default 'user',
+    phone_number text
+);
+
+create table projects (
+    id bigint GENERATED ALWAYS AS IDENTITY,
+    title text not null,
+    description text not null,
+    github_repo text not null,
+    public boolean not null default true
+);
